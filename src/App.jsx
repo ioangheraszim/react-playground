d92c7e7234df691d343ed92c455c8fcf57085ed4
ioangheraszim <gijohny17@gmail.com>
2025-06-20 10:18:22 +0100

    Exercise: build a counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import './App.css';
-
-function ListItem(props) {
-  return <li>{props.animal}</li>;
-}
-
-function List(props) {
-  return (
-    <ul>
-      {props.animals.map((animal) => {
-        return <ListItem key={animal} animal={animal} />;
-      })}
-    </ul>
-  );
-}
+import { useState } from 'react';
 
 function App() {
-  const animals = ['Lion', 'Cow', 'Snake', 'Lizard'];
+  const [number, setNumber] = useState(0);
+
+  const increaseNumber = () => {
+    setNumber((number) => number + 1);
+  };
+
+  const decreaseNumber = () => {
+    setNumber((number) => number - 1);
+  };
 
   return (
     <>
-      <h1>Animals: </h1>
-      <List animals={animals} />
+      <div>
+        <button onClick={increaseNumber}>Next</button>
+        <p>{number}</p>
+        <button disabled={number === 0} onClick={decreaseNumber}>
+          Prev
+        </button>
+      </div>
     </>
   );
 }
